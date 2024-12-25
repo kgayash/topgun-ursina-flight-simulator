c50a242e2174ea17dab9a03b7f7c3dd70db50ac6
AirRacer <airracer@AirRacers-MacBook-Pro.local>
2024-12-24 17:05:51 -0800

    Added code file for building private FA-18 fighter aircraft-will be imported from Blender in January 2025

---
+++ 18fighter_aircraft_blender.py
@@ -0,0 +1,9 @@
+from ursina import *
+
+# Initialize the Ursina application
+app = Ursina()
+
+#Will be importing 3D private U.S.A.F FA/18 from Blender in January, 2025
+
+# Run the application
+app.run()
